176fd931a5bf65084495fc059138e4ec2a532258
Mourya5AL <mouryagoguri4@gmail.com>
2025-03-18 12:42:57 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,19 @@
+function LoginValidate(){
+    var enteremail=document.getElementById('email'),value;
+    var enterpassword=document.getElementById('pwd'),value;
+
+    var getEmail=localStorage.getItem('email')
+    var getPwd=localStorage.getItem('password')
+    if(enteremail==getEmail){
+        if(enterpassword==getPwd){
+            alert("login success");
+            window.location="welcome.html";
+            return false;
+        }
+        else{
+            alert('worng password');
+        }
+    }else{
+        alert("invalid details");
+    }
+}
